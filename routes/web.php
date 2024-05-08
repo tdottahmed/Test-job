0b8e1991a721ccec32bec0f1aca03afc1c6dfedb
Tanbir Ahmed <pondit.tanbir@gmail.com>
2024-05-09 01:22:52 +0600

    redirect route for welcome page change

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\ColorController;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('product.index');
 });
 
 Route::resource('/unit', UnitController::class);
